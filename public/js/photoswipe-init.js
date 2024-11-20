d468453c7fef0e95f5ee09e30d08ccf04f5aaaae
Senk Ju <12718920+oskar2517@users.noreply.github.com>
2024-11-20 16:57:30 +0100

    remove public folder

--- public/js/photoswipe-init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const lightbox = new PhotoSwipeLightbox({
-        gallery: ".gallery",
-        pswpModule: PhotoSwipe
-    });
-    lightbox.init();
-
-    console.log("init")
-});
