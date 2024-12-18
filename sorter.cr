d5266fcca9760fd61abc0f49923125a1d40538ec
Senk Ju <12718920+oskar2517@users.noreply.github.com>
2024-12-18 17:46:25 +0100

    improve collection sorter

--- sorter.cr
+++ sorter.cr
@@ -3,8 +3,7 @@ FILE = "static/collection.txt"
 lines = File.read_lines(FILE)
 
 sorted = lines.slice_when(&.empty?)
-    .map { |g| [g.first, *g[1..].reject(&.empty?).sort_by { |i| i.split[2] }, " "] }
-    .flatten
+    .flat_map { |g| [g.first, *g[1..].reject(&.empty?).sort_by { |i| i.split[2..] }, ""] }
     .join("\n")
 
 File.write(FILE, sorted)
